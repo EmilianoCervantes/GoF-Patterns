df264c965e72e83d4ddccf1508be061ecb1194dd
Emiliano Cervantes <ovman96@hotmail.com>
2018-02-27 20:51:32 -0600

    First Commit

---
+++ FactoryMethod.java
@@ -0,0 +1,12 @@
+
+public interface LeectorCodigo{
+	public String getCodigo();
+}
+
+public class LeectorBarras implements LeectorCodigo{
+
+}
+
+public class Leector implements LeectorCodigo{
+
+}
