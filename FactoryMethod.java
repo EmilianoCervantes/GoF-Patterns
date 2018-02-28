039f1c811c7282722a56a0d289db714401cd99d3
Emiliano Cervantes <ovman96@hotmail.com>
2018-02-27 21:05:55 -0600

    Basic FactoryMethod in java

--- FactoryMethod.java
+++ FactoryMethod.java
@@ -4,9 +4,27 @@ public interface LeectorCodigo{
 }
 
 public class LeectorBarras implements LeectorCodigo{
+	private String barsCode;
 
+	public LeectorBarras(String code){
+		this.barsCode = code;
+	}
+
+	@Override
+  public String getCodigo() {
+      return barsCode;
+  }
 }
 
-public class Leector implements LeectorCodigo{
+public class LeectorQr implements LeectorCodigo{
+	private String qrCode;
 
+	public LeectorQr(String code){
+		this.qrCode = code;
+	}
+
+	@Override
+	public String getCodigo() {
+      return qrCode;
+  }
 }
